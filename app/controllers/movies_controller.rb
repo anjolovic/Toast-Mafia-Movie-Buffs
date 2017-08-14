e3f871a3523472e0cae49e7853045bfe332d020b
Alex Jolovic <ajolovic@me.com>
2017-08-14 12:07:15 -0700

    Webpack: Run webpacker react generator - Webpacker-React makes it easy to use React with Webpacker in your Rails applications.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,36 @@
 class MoviesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+    current_sort_order = session[:sort_order] || sort_order
+    @movies = serialize_collection(Movie.order(current_sort_order)
+                                    .page(page_number)
+                                    .includes(:genres, :reviews))
+  end
+
+  def set_sort_order
+    session[:sort_order] = sort_order
+    head :ok
+  end
+
+  private
+
+  def serialize_collection(movies)
+    ActiveModel::Serializer::CollectionSerializer.new(movies,
+                                                      each_serializer: MovieSerializer)
+      .as_json
+  end
+
+  def page_number
+    params[:page] || 1
+  end
+
+  def sort_order
+    order_map = {
+      title: 'title ASC',
+      release_date: 'release_date DESC',
+      popularity: 'popularity DESC'
+    }
+    order_map.fetch("#{params[:sort_order]}".to_sym, order_map[:title])
   end
 end
