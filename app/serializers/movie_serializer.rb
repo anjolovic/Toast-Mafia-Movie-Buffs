561601a6cb0a34f0799441a795cce73a57979778
Alex Jolovic <ajolovic@me.com>
2017-08-20 13:46:21 -0700

    Updated movie serializer and added review serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,11 +1,23 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :vote_count, :title, :poster_path, :release_date, :genres, :reviews
+  attributes :id, :vote_count, :title, :poster_path, :release_date, :genres, :reviews, :rating
 
   def genres
     object.genres.map(&:name)
   end
 
-  def reviews
-    object.reviews.map{| review | [review.id, review.content]}
+  def rating
+    reviews = object.reviews.where.not(rating: nil)
+    review_count = reviews.count
+    if review_count > 0
+      round_to_nearest_point_five(reviews.sum(&:rating)/review_count)
+    else
+      0
+    end
+  end
+
+  private
+
+  def round_to_nearest_point_five(value)
+    ((value * 2).ceil)/2.0
   end
 end
