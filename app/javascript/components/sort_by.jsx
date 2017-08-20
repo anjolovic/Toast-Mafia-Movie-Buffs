d178f64b8bc969a646b6d97dfe3df915a5d46ec7
Alex Jolovic <ajolovic@me.com>
2017-08-20 13:49:20 -0700

    Updated sort by component

--- app/javascript/components/sort_by.jsx
+++ app/javascript/components/sort_by.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {ButtonGroup, Button} from 'react-bootstrap';
 
 class SortBy extends React.Component {
   constructor(props) {
     super(props);
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.state = {
+      sortOrder: this.getSortOrder(this.props.sortOrder)
+    };
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({sortOrder: this.getSortOrder(nextProps.sortOrder)});
+  }
+
+  classNames (buttonType) {
+    const activeButton = this.state.sortOrder === buttonType ? ' activeButton' : '';
+    return "btn btn-secondary" + activeButton;
+  }
+
+  getSortOrder (sortOrder) {
+    return sortOrder.length > 1 ? sortOrder.split(' ')[0] : 'title';
   }
 
   handleButtonClick (e) {
@@ -14,26 +31,27 @@ class SortBy extends React.Component {
 
   render () {
     const labelStyle = {
-      padding: '15px 20px 0'
+      padding: '15px 25px 0'
     };
+    console.log(this.state.sortOrder);
 
     return (
-      <section className="sort-by SortBy mt-4">
-        <div className="btn-toolbar"  role="toolbar">
+      <section className="sort-by SortBy mt-4 mb-4">
+        <ButtonGroup role="group" aria-label="sort" bsSize="xsmall">
           <label style={labelStyle}>Sort By:</label>
-
-          <div className="btn-group" role="group" aria-label="sort">
-            <button type="button" className="btn btn-secondary" data-sort-by="title" onClick={ this.handleButtonClick }>
-              Title
-            </button>
-            <button type="button" className="btn btn-secondary" data-sort-by="release_date" onClick={ this.handleButtonClick }>
-              Released Date
-            </button>
-            <button type="button" className="btn btn-secondary" data-sort-by="popularity" onClick={ this.handleButtonClick }>
-              Popularity
-            </button>
-          </div>
-        </div>
+          <Button
+            className={ this.classNames("title") }
+            data-sort-by="title"
+            onClick={ this.handleButtonClick }>Title</Button>
+          <Button
+            className={ this.classNames("release_date") }
+            data-sort-by="release_date"
+            onClick={ this.handleButtonClick }>Released Date</Button>
+          <Button
+            className={ this.classNames("popularity") }
+            data-sort-by="popularity"
+            onClick={ this.handleButtonClick }>Popularity</Button>
+        </ButtonGroup>
       </section>
     );
   }
